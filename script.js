13345038f859a46744ce6e995570d6256c827ecf
Jessica Anocibar <jessicayaws@yahoo.com>
2020-09-29 09:52:26 -0600

    added files

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and 
+// I am presented with a question     <<---how does this happen?!?!?>>
+// WHEN I answer a question correctly    <<HOW WILL THE CODE KNOW WHICH ANSWER IS CORRECT?>>
+// THEN I am presented with another question   <<--FUNCTION>>
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0    <<--FUNCTION AGAIN  >>
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+
+
+// need to display a code quiz that prompts the user to "start quiz"
+// need to input a timer 
+
+// present question using a function
+// user answers it correctly
+// then present user with another question using a function
+// user answers incorrectly
+// then subtract time from the clock
+// when all questions are answered or timer reaches 0 add in another function
+// then the game ends 
+// then prompt user to save initials and score 
+
+
+
