cd45bb9926340cfabc18a144d4d3714dc36a3221
Jessica Anocibar <jessicayaws@yahoo.com>
2020-09-29 20:50:04 -0600

    modified html and js

--- script.js
+++ script.js
@@ -27,5 +27,67 @@
 // then the game ends 
 // then prompt user to save initials and score 
 
+//for quiz state
+var questionIndex = 0
+var time = questions.length * 30;
+var timerId;
 
+
+var questions = [{ title: "Commonly used data types don't include:", 
+                   choices: ["strings", "alerts", "booleans", "numbers"],
+                   answer: "alerts"
+                 },
+                 
+
+                 { title: "The condition in an if/else statement is enclosed within ___?",
+                   choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+                   answer: "parentheses"
+                },
+
+
+                { title: "Arrays in Javascript can be used to store __?",
+                  choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+                  answer: "all of the above"
+                },
+
+
+                { title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+                  choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+                  answer: "console.log"
+                }
+
+                
+                ];
+
+
+//variables for the DOM elements
+var beginBtn = document.getElementById("begin");
+var submitBtn = document.getElementById("submit");
+var initialsEl = document.getElementById("initials");
+var timerEl = document.getElementById("timer");
+var choicesEl = document.getElementById("choices");
+var feedbackEl = document.getElementById("feedback");
+var questionsEl = document.getElementById("questions");
+
+
+
+function beginQuiz(){
+   var startScreenEl = document.getElementById("start-screen");
+   startScreenEl.setAttribute("class", "hide");
+   questionsEl.removeAttribute("class");
+
+   timerId = setInterval(clockTick, 1000);
+   timerEl.textContent = time;
+
+   startQuestion();
+  }
+  
+
+  function startQuestion () {
+
+  }
+  
+  
+  
+ 
 
